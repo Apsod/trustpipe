8247823e9d4c7f9a053b5c3474780fffd6f237dc
cubagyllensten1 <cubagyllensten1@judac07.zam.kfa-juelich.de>
2024-02-29 13:29:45 +0100

    fixed judac stuff

--- example/process/process.py
+++ example/process/process.py
@@ -2,13 +2,17 @@ import re
 import json
 import itertools
 import sys
+import os
+import gzip
+from multiprocessing import Pool, Queue
+
 import pypandoc
 
 end_of_prefix = re.compile(r'^\*{3} START OF THE PROJECT GUTENBERG EBOOK .*? \*{3}$')
 start_of_suffix = re.compile(r'^\*{3} END OF THE PROJECT GUTENBERG EBOOK .*? \*{3}$')
 
-def process_epub(file):
-    doc = pypandoc.convert_file(file, 'plain', format='epub', extra_args=['--wrap=preserve'])
+def process_epub(data):
+    doc = pypandoc.convert_text(data, 'plain', format='epub', extra_args=['--wrap=preserve'])
     lines = iter(doc.split('\n'))
 
     prefix = []
@@ -35,10 +39,53 @@ def process_epub(file):
 
     suffix = '\n'.join(suffix)
 
-    return prefix, text, suffix
+    return {'prefix': prefix, 'text': text, 'suffix': suffix}
+
+def reader(root, queue):
+    for de in os.scandir(root):
+        if de.name.endswith('epub.noimages'):
+            with open(de, 'rb') as handle:
+                queue.put(handle.read())
+
+def worker(in_queue, out_queue):
+    while (data := in_queue.get()):
+        out_queue.put(json.dumps(process_epub(data)))
+    
+
+def writer(dest, in_queue):
+    with gzip.open(dest, 'w') as out:
+        while (data := in_queue.get()):
+            out.write(data)
+            out.write('\n')
 
 if __name__ == '__main__':
-    prefix, text, suffix = process_epub(sys.argv[1])
-    print(json.dumps(
-        dict(prefix=prefix, text=text, suffix=suffix)
-    ))
+    root = sys.argv[1]
+    out = sys.argv[2]
+    P = max(8, os.cpu_count())
+
+    raw_queue = Queue(P*2)
+    formatted_queue = Queue(P*2)
+    
+    reader_process = Process(target=reader, args=(root, raw_queue))
+    worker_processes = [Process(target=worker, args=(raw_queue, formatted_queue)) for _ in range(P)]
+    writer_process = Process(target=writer, args=(out, formatted_queue,))
+
+    reader_process.start()
+    for worker in worker_processes:
+        worker.start()
+    writer_process.start()
+
+    # When all has been read
+    # Send stop signal to workers
+    reader_process.join()
+    for _ in worker_processes:
+        raw_queue.put(None)
+    
+    # When all workers are finished
+    # Send stop signal to writer
+    for worker in worker_processes:
+        worker.join()
+    formatted_queue.put(None)
+    
+    # Wait for writer to finish
+    writer_process.join()
--- example/process/process.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-OUT=/output/gutenberg.json
-IN=/input
-
-parallel python process.py ::: $IN/*.epub.noimages > $OUT
-
-gzip $OUT
